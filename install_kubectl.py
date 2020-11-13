153d85e423d756ff7599840a8489a971d307dc7f
Petyo Kunchev <ptkunchev@gmail.com>
2020-11-13 15:44:56 +0200

    updated install_kubectl.py

--- install_kubectl.py
+++ install_kubectl.py
@@ -1,15 +1,42 @@
 #! /usr/bin/env python3
+# -*- coding: utf-8 -*-
 
-"""This script will install kubectl latest on GNU/Linux."""
+"""This script will install kubectl latest on GNU/Linux.
+
+- The 'kubectl' tool controls the Kubernetes cluster:
+https://kubernetes.io/docs/reference/kubectl/kubectl/
+https://kubernetes.io/docs/reference/kubectl/overview/
+
+- Linux is a family of open-source Unix-like operating systems based on
+the Linux kernel:
+https://www.kernel.org/category/about.html
+
+- Python is an interpreted, high-level, dynamically typed,
+garbage-collected and general-purpose programming language:
+https://en.wikipedia.org/wiki/Python_Software_Foundation_License
+https://docs.python.org/3/license.html
+"""
+
+__author__ = 'Petyo Kunchev'
+__version__ = '1.0.0'
+__maintainer__ = 'Petyo Kunchev'
+__email__ = 'ptkunchev@gmail.com'
+__license__ = 'MIT'
 
 import os
-import requests
 import urllib.request
 import urllib.error
 import shutil
 import subprocess
 from sys import platform
 from requests.models import Response
+
+try:
+    import requests
+    from requests.models import Response
+except ModuleNotFoundError as err:
+    print('Please install the required modules.')
+    exit(err)
 
 
 class KubectlInstallation:
@@ -32,8 +59,17 @@ class KubectlInstallation:
     # local folder to move the kubectl binary package (must be in $PATH)
     binlocation: str = '/usr/local/bin/kubectl'
 
-    def get_kubectl(self) -> object:
+    @property
+    def running_os(self) -> object:
+        return platform
+
+    @property
+    def running_id(self) -> object:
+        return os.getuid()
+
+    def get_kubectl(self):
         """Download and install the latest kubectl for Linux."""
+
         # 1. download kubectl to temporary location
         print(f'[STEP 1]: Starting download of the latest kubectl version'
               f' \'{self.latest_kube.text}\' from:\n{self.url}')
@@ -60,8 +96,33 @@ class KubectlInstallation:
 
 
 def main():
+    """Wrap up everything into the main function."""
+
+    # create object from the KubectlInstallation class
     this_setup = KubectlInstallation()
-    this_setup.get_kubectl()
+
+    # define the required operating system (Linux)
+    rq_os: str = 'linux'
+
+    # define required user id (root)
+    rq_id: str = '0'
+
+    # get currently running os
+    rn_os: str = str(this_setup.running_os)
+
+    # get currently running user
+    rn_id: str = str(this_setup.running_id)
+
+    # check if operating system and user id requirements are met
+    if rn_os != rq_os or rn_id != rq_id:
+        exit(f'[ERROR]: Running on {rn_os} with {rn_id}, must run on {rq_os} '
+             f'with user id {rq_id} please check. Exiting...')
+    else:
+        # if conditions are met, run the installation
+        print(f'Install version: {this_setup.latest_kube.text}')
+        print(f'Operating system is: {rn_os}')
+        print(f'User ID is: {rn_id}')
+        this_setup.get_kubectl()
 
 
 if __name__ == '__main__':
